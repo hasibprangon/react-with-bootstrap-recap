2f62d9555b39def9ec76ef28b6562b326d293023
hasibprangon <hasibulhossainp09@gmail.com>
2023-11-28 19:13:29 +0600

    bootstrap regular system

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" >
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -28,6 +28,48 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <p>
+        <button type="button" className="btn btn-primary">Primary</button>
+        <button type="button" className="btn btn-secondary">Secondary</button>
+        <button type="button" className="btn btn-success">Success</button>
+        <button type="button" className="btn btn-danger">Danger</button>
+        <button type="button" className="btn btn-warning">Warning</button>
+        <button type="button" className="btn btn-info">Info</button>
+        <button type="button" className="btn btn-light">Light</button>
+        <button type="button" className="btn btn-dark">Dark</button>
+
+        <button type="button" className="btn btn-link">Link</button>
+      </p>
+
+      <div>
+        <div className="card-group">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+              </div>
+          </div>
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+              </div>
+          </div>
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+              </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
